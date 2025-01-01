c5555e11aaa9778d203435448420b21baafd5bb0
KangWon LEE <kangwon@gmail.com>
2025-01-01 22:39:39 +0900

    specify bash for run_30.sh
    set -e

--- run_30.sh
+++ run_30.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 set -e # exit on any error
 # https://github.com/f1tenth/f1tenth_gym_ros?tab=readme-ov-file#launching-the-simulation
 # to start the simulator
 cd ~/sim_ws
-. /opt/ros/foxy/setup.bash
-. ~/sim_ws/install/local_setup.bash
+source /opt/ros/foxy/setup.bash
+source ~/sim_ws/install/local_setup.bash
 
 ros2 launch f1tenth_gym_ros gym_bridge_launch.py &
