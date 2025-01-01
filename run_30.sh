b42ab757b91729260e5dd67397050e0a5d4ece91
KangWon LEE <kangwon@gmail.com>
2025-01-01 21:40:26 +0900

    specify path to local_setup.bash

--- run_30.sh
+++ run_30.sh
@@ -1,7 +1,8 @@
+set -e # exit on any error
 # https://github.com/f1tenth/f1tenth_gym_ros?tab=readme-ov-file#launching-the-simulation
 # to start the simulator
 cd ~/sim_ws
 source /opt/ros/foxy/setup.bash
-source install/local_setup.bash
+source ~/sim_ws/install/local_setup.bash
 
 ros2 launch f1tenth_gym_ros gym_bridge_launch.py &
