a34381ac1f3561f8a5e3f34539bbc4fa602fd6e3
KangWon LEE <kangwon@gmail.com>
2025-01-01 22:34:03 +0900

    replace `source` with `.`
    sometimes `.` works but not `source`

--- run_30.sh
+++ run_30.sh
@@ -2,7 +2,7 @@ set -e # exit on any error
 # https://github.com/f1tenth/f1tenth_gym_ros?tab=readme-ov-file#launching-the-simulation
 # to start the simulator
 cd ~/sim_ws
-source /opt/ros/foxy/setup.bash
-source ~/sim_ws/install/local_setup.bash
+. /opt/ros/foxy/setup.bash
+. ~/sim_ws/install/local_setup.bash
 
 ros2 launch f1tenth_gym_ros gym_bridge_launch.py &
